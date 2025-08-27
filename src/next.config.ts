d4b02cde0d6cf575e956c97a9be30774c24f8597
Pog Coin <info.pogcoin@gmail.com>
2025-08-27 21:13:48 +0000

    Error: Invalid src prop (https://picsum.photos/48/48) on `next/image`, h

---
+++ src/next.config.ts
@@ -0,0 +1,68 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'saaswheel.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+    // Allow data URIs
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    contentDispositionType: 'inline',
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', // Increase for image uploads
+      serverActionsTimeout: 120, 
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        'firebase-admin': false,
+      };
+    }
+    // This is the new rule to handle the opentelemetry issue
+    config.externals.push('@opentelemetry/winston-transport');
+    return config;
+  },
+};
+
+export default nextConfig;
